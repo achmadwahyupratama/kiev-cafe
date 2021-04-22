2cbc0af515f7ab84c6e7cbea8f72ae107cef8176
LiliAmber <dmraraa@gmail.com>
2021-04-22 23:30:22 +0700

    list order

--- controllers/custController.js
+++ controllers/custController.js
@@ -1,15 +1,28 @@
-
+const { Food, Customer, Order } = require('../models/index')
 class custController {
     static listOrder(req, res) {
-        res.send('ini cust controller')
+        // res.send('ini cust controller')
+        // console.log(req.session.customerId)
+        let id = +req.session.customerId
+        Customer.findAll({
+            include: Food
+        })
+            .then((data) => {
+                console.log(data)
+                res.render('listOrder')
+            })
+            .catch((err) => {
+                res.send(err.message)
+            })
     }
 
     static checkout(req, res) {
         res.send('ini cekout')
     }
-
+    
     static getQtyUpdate(req, res) {
-        res.send('ini apdet')
+        // res.send('ini apdet')
+        res.render('editOrder')
     }
 
     static postQtyUpdate(req, res) {
